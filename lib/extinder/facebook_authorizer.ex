d1f8257a401657c6a8b93f5ad01818159bfd3697
Thomas Graves <thomas@ooo.pm>
2016-05-07 22:50:47 -0400

    Raise exception when facebook login fails

--- lib/extinder/facebook_authorizer.ex
+++ lib/extinder/facebook_authorizer.ex
@@ -1,5 +1,6 @@
 defmodule ExTinder.FacebookAuthorizer do
-    use Hound.Helpers
+  use Hound.Helpers
+  alias ExTinder.FacebookTokenError
 
   @auth_url "https://www.facebook.com/dialog/oauth?client_id=464891386855067&redirect_uri=https://www.facebook.com/connect/login_success.html&scope=basic_info,email,public_profile,user_about_me,user_activities,user_birthday,user_education_history,user_friends,user_interests,user_likes,user_location,user_photos,user_relationship_details&response_type=token"
 
@@ -26,6 +27,13 @@ defmodule ExTinder.FacebookAuthorizer do
   end
 
   def extract_token_from_url(url) do
-    Regex.run(~r/=(.*?)&/, url) |> Enum.at(1)
+    token = Regex.run(~r/=(.*?)&/, url) |> Enum.at(1)
+
+    if String.length(token) == 1 do
+      raise FacebookTokenError, code: token,
+      message: "Couldn't extract token, check email/password."
+    else
+      token
+    end
   end
 end
